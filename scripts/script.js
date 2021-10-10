89629cdd6409800838b5b7557ca3e43d5e610d5a
SiveyDev <sivey@protonmail.com>
2021-10-09 20:18:09 -0400

    Update computerPlay function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
-console.log("Hello");
-
 let computerSelection = computerPlay();
 
 console.log(computerSelection);
 
 function computerPlay() {
     rand = Math.floor(Math.random() * 3) + 1;
-    return rand;
+    // console.log(rand);
+    if (rand == 1) {
+        return "rock";
+    } else if (rand == 2) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
 }
