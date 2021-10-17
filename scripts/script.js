ca4704ac3937bfe8cc630509aae88277a8bcc72f
SiveyDev <sivey@protonmail.com>
2021-10-17 14:38:52 -0400

    Add div to display results of the game

--- scripts/script.js
+++ scripts/script.js
@@ -9,43 +9,46 @@
 
 const buttons = Array.from(document.querySelectorAll('button'));
 buttons.forEach(button => button.addEventListener('click', playRound));
+const result = document.querySelector('#result');
 
 function playRound(e) {
     let computerSelection = computerPlay();
     let btnSelect = document.querySelector(`button[id="${e.target.id}"]`);
     let playerSelection = btnSelect.id;
+    console.log("Computer selection is: " + computerSelection);
+    console.log("Player selection is: " + playerSelection);
     
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            console.log("Tie! No one wins");
+            result.textContent = "Tie! No one wins";
         }
         if (computerSelection == "paper") {
-            console.log("You Lose! Paper beats rock");
+            result.textContent = "You Lose! Paper beats rock";
         }
         if (computerSelection == "scissors") {
-            console.log("You Win! Rock beats scissors");
+            result.textContent = "You Win! Rock beats scissors";
         }
     }
     if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            console.log("You Win! Paper beats rock");
+            result.textContent = "You Win! Paper beats rock";
         }
         if (computerSelection == "paper") {
-            console.log("Tie! No one wins");
+            result.textContent = "Tie! No one wins";
         }
         if (computerSelection == "scissors") {
-            console.log("You Lose! Scissors beats paper");
+            result.textContent = "You Lose! Scissors beats paper";
         }
     }
     if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            console.log("You Lose! Rock beats scissors");
+            result.textContent = "You Lose! Rock beats scissors";
         }
         if (computerSelection == "paper") {
-            console.log("You Win! Scissors beats paper");
+            result.textContent = "You Win! Scissors beats paper";
         }
         if (computerSelection == "scissors") {
-            console.log("Tie! No one wins");
+            result.textContent = "Tie! No one wins";
         }
     }
 }
